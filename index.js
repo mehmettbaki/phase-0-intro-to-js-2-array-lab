15bdc5ea55a3e5f01a1c33f56d62d12fd9686b0c
memo <mehmetbaki.123@gmail.com>
2022-04-10 20:35:12 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+
+function appendCat(name) {
+    const newCats=[...cats]
+    newCats.push(name)
+    return newCats
+}
+
+function removeFirstCat() {
+    const copyCats=[...cats]
+    copyCats.shift()
+    return copyCats
+}
+
+function removeLastCat(){
+    const copyCats=[...cats]
+    copyCats.pop()
+    return copyCats
+}
+
+function prependCat(name) {
+    const copCats=[...cats]
+    copCats.unshift(name)
+    return copCats
+}
